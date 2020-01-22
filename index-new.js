c247a8fb3c137fd1e9189f09df6c99d448291869
Joey Ballentine <joeyjballentine@gmail.com>
2020-01-22 01:01:22 -0600

    More rewrite stuff

--- index-new.js
+++ index-new.js
@@ -1,57 +1,57 @@
 // Discord.js import
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const request = require(`request`);
 
 // File stuff
 const fs = require(`fs`);
-const fsExtra = require("fs-extra");
+const fsExtra = require('fs-extra');
 
 // Image downloading stuff
-const isImageUrl = require("is-image-url");
-const download = require("image-downloader");
+const isImageUrl = require('is-image-url');
+const download = require('image-downloader');
 
 // Python shell
-const { PythonShell } = require("python-shell");
+const { PythonShell } = require('python-shell');
 
 // The image upscale queue
 // This uses an array instead of a map as it must be the same queue across all servers
 const queue = new Array();
 
 // The prefix used for commands
-const prefix = "--";
+const prefix = '--';
 
 // Change these depending on what you want to allow
 const pixelLimit = 500 * 500;
 const sizeLimit = 500000;
 
 // Path to ESRGAN. Should be initialized by a submodule and is meant to be used with BlueAmulet's fork
-const esrganPath = "./ESRGAN/";
+const esrganPath = './ESRGAN/';
 
 // Connects to the bot account and empties the directories
-client.on("ready", () => {
+client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     emptyDirs();
 });
 
 // Message event handler
-client.on("message", async message => {
+client.on('message', async message => {
     // Removes extra spaces between commands
-    message = message.replace(/ +(?= )/g, "");
+    message = message.replace(/ +(?= )/g, '');
 
     // The bot will not respond unless the prefix is typed
     // It will also ignore anything sent by itself
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     // Splits the args into an array
-    const args = message.content.slice(prefix.length).split(" ");
+    const args = message.content.slice(prefix.length).split(' ');
 
     // Strips the command off of the args array
     const command = args.shift().toLowerCase();
 
     // Does all the steps required for upscaling the image
-    if (command === "upscale") {
+    if (command === 'upscale') {
         // If no args are given the bot will stop and send an error message
         if (!args.length) {
             return message.channel.send(
@@ -68,18 +68,18 @@ client.on("message", async message => {
             url = args.shift();
         } else {
             // If no image is given the bot will error
-            return message.channel.send("Not a valid command.");
+            return message.channel.send('Not a valid command.');
         }
 
         // Downloads the image and returns an filename & image
         let image = downloadImage(url);
 
         // Gets the model name from the model argument
-        let model = args[0].includes(".pth") ? args[0] : args[0] + ".pth";
+        let model = args[0].includes('.pth') ? args[0] : args[0] + '.pth';
 
         // Checks to make sure model name is valid (exists and is spelled right)
-        if (!fs.readdirSync(esrganPath + "/models/").includes(model)) {
-            return message.channel.send("Not a valid model.");
+        if (!fs.readdirSync(esrganPath + '/models/').includes(model)) {
+            return message.channel.send('Not a valid model.');
         }
 
         // The job sent for processing
@@ -88,23 +88,24 @@ client.on("message", async message => {
             image: image,
             resize: false,
             filter: false,
-            montage: false
+            montage: false,
+            message: message
         };
 
         // Parsing the extra arguments
 
         // Resize
-        if (["--resize", "-r"].some(arg => args.includes(arg))) {
+        if (['--resize', '-r'].some(arg => args.includes(arg))) {
             upscaleJob.resize = args[args.indexOf(arg) + 1];
         }
 
         // filter
-        if (resize && ["--filter", "-f"].some(arg => args.includes(arg))) {
+        if (resize && ['--filter', '-f'].some(arg => args.includes(arg))) {
             upscaleJob.filter = args[args.indexOf(arg) + 1];
         }
 
         // Montage
-        if (["--montage", "-m"].some(arg => args.includes(arg))) {
+        if (['--montage', '-m'].some(arg => args.includes(arg))) {
             upscaleJob.montage = args[args.indexOf(arg) + 1];
         }
 
@@ -139,20 +140,20 @@ client.on("message", async message => {
 });
 
 function emptyDirs() {
-    fsExtra.emptyDirSync(esrganPath + "/results/");
-    fsExtra.emptyDirSync(esrganPath + "/LR/");
+    fsExtra.emptyDirSync(esrganPath + '/results/');
+    fsExtra.emptyDirSync(esrganPath + '/LR/');
 }
 
 function downloadImage(url) {
     const options = {
         url: url,
-        dest: esrganPath + "./LR"
+        dest: esrganPath + './LR'
     };
 
     download
         .image(options)
         .then(({ filename, image }) => {
-            console.log("Saved to", filename);
+            console.log('Saved to', filename);
             console.log(image);
             return {
                 filename,
@@ -171,12 +172,54 @@ function process(job) {
     optimize();
 
     if (job.montage) montage();
-    return;
+
+    return job.message
+        .reply(`Upscaled using ${job.model}`, {
+            files: [`${esrganPath}/results/${name}_rlt.png`]
+        })
+        .then(() => {
+            queue.shift();
+            try {
+                process(queue[0]);
+            } catch (err) {
+                console.log(err);
+                queue = [];
+                return job.message.channel.send(err);
+            }
+        });
 }
 
-function upscale() {
-    // Clear folders (just in case)
-    emptyDirs();
+function upscale(model) {
+    let args = {
+        args: [
+            `${esrganPath}/models/${model}`,
+            `--input=${esrganPath}/LR/`,
+            `--output=${esrganPath}/results/`
+        ]
+    };
+    PythonShell.run(esrganPath + '/test.py', args, (err, results) => {
+        if (err) {
+            console.log(err);
+            queue = [];
+            return message.channel.send(
+                'Sorry, there was an error processing your image.'
+            );
+        }
+
+        let filePath = `${esrganPath}/results/${name}_rlt.png`;
+
+        try {
+            if (!fs.existsSync(filePath)) {
+                return message.channel.send(
+                    'Sorry, there was an error processing your image.'
+                );
+            }
+        } catch (err) {
+            return message.channel.send(
+                'Sorry, there was an error processing your image.'
+            );
+        }
+    });
 }
 
 function resize() {}
@@ -191,4 +234,19 @@ function merge() {
     //TODO
 }
 
-function optimize() {}
+function optimize() {
+    const imagemin = require('imagemin');
+    const imageminOptipng = require('imagemin-optipng');
+
+    (async () => {
+        await imagemin(
+            [`${esrganPath}/results/*.png`],
+            `${esrganPath}/results/`,
+            {
+                use: [imageminOptipng()]
+            }
+        );
+
+        console.log('Images optimized!');
+    })();
+}
