a48920a43185ebf499f65d3e80c6b5f4ec6b0b7a
MrTeferi <freethoughtleft@gmail.com>
2023-01-28 03:26:18 -0600

    fix(pixel_unshuffle): Allow images with dimensions not divisible by 2.
    Allows some x2 models that require dimensions divisible by 2 to work properly.

--- utils/architecture/RRDB.py
+++ utils/architecture/RRDB.py
@@ -9,6 +9,7 @@ from collections import OrderedDict
 import torch
 import torch.nn as nn
 import utils.architecture.block as B
+import torch.nn.functional as F
 
 
 # Borrowed from https://github.com/rlaphoenix/VSGAN/blob/master/vsgan/archs/ESRGAN.py
@@ -239,5 +240,15 @@ class RRDBNet(nn.Module):
 
     def forward(self, x):
         if self.shuffle_factor:
+            _, _, h, w = x.size()
+            mod_pad_h = (
+                                self.shuffle_factor - h % self.shuffle_factor
+                        ) % self.shuffle_factor
+            mod_pad_w = (
+                                self.shuffle_factor - w % self.shuffle_factor
+                        ) % self.shuffle_factor
+            x = F.pad(x, (0, mod_pad_w, 0, mod_pad_h), "reflect")
             x = torch.pixel_unshuffle(x, downscale_factor=self.shuffle_factor)
+            x = self.model(x)
+            return x[:, :, : h * self.scale, : w * self.scale]
         return self.model(x)
